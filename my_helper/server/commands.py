53a189e104725426411211b742a2b64100d43f24
Ivan <kalent_ivan@mail.ru>
2021-02-09 06:36:59 +0300

    server

--- my_helper/server/commands.py
+++ my_helper/server/commands.py
@@ -6,3 +6,9 @@ ser_connect_to_app = 202
 app_test_cmd = 2
 ser_test_cmd = 203
 nb_test_cmd = 102
+
+to_app = 3
+to_nb = 103
+
+from_app = 4
+from_nb = 104
--- my_helper/server/my_server.py
+++ my_helper/server/my_server.py
@@ -10,38 +10,34 @@ import threading
 class Server:
     def __init__(self):
         self.my_queue = deque()
-        self.sock.listen(2)
-        self.conn = dict(sock_app=0, sock_nb=0, adr_app=0, adr_nb=0, thr_app=0, thr_nb=0)
-        self.thr_queue = 0
-        self.new_mess = False
+        self.sock = socket.socket()
+        self.conn = dict(sock_app=None, sock_nb=None, adr_app=None, adr_nb=None, thr_app=None, thr_nb=None)
         logging.basicConfig(filename="log_file.log", level=logging.INFO)
         self.config = configparser.ConfigParser()
         self.config.read('../config.ini')
         self.size_next_mess = int(self.config.get("server", "size_first_mes"))
-        logging.info("Сервер запущен")
+        logging.info("Сервер создан")
 
     def close(self):
         self.conn['sock_app'].close()
         self.conn['sock_nb'].close()
 
-    def send_message(self, cmd, data, where):
-        message = dict(cmd=cmd, data=data, size_next_message=10)
-        zip_data = pickle.dumps(message)
-        #self.conn.send(zip_data)
+    def send_message(self, cmd, data, where, size_next=1024):
+        message = dict(cmd=cmd, data=data, size_next_message=size_next)
+        zip_message = pickle.dumps(message)
+        key = 'sock_app' if where == to_app else 'sock_nb'
+        self.conn[key].send(zip_message)
 
     def listener(self, source):
-        if source == "app":
-            conn, adr = self.conn['sock_app'], self.conn['adr_nb']
-        elif source == "nb":
-            conn, adr = self.conn['sock_nb'], self.conn['adr_nb']
-
+        key = 'sock_app' if source == from_app else 'sock_nb'
+        conn, adr = self.conn[key], self.conn[key]
         while True:
-            receive_data = pickle.loads(conn.recv(self.size_next_mess))
-            if not receive_data:
+            message = pickle.loads(conn.recv(self.size_next_mess))
+            if not message:
                 continue
             else:
-                self.my_queue.append(receive_data)
-                self.queue_worker()
+                self.my_queue.append(message)
+                threading.Thread(target=self.queue_worker()).start()
 
     def queue_worker(self):
         message = self.my_queue.pop()
@@ -56,22 +52,21 @@ class Server:
             pass
 
     def run(self):
-        self.sock = socket.socket()
         self.sock.bind(('', 9090))
-        self.thr_queue = threading.Thread(target=self.queue_worker())
-        self.thr_queue.start()
+        self.sock.listen(2)
         while True:
             conn, adr = self.sock.accept()
-            receive_data = pickle.loads(conn.recv(self.size_next_mess))
-            if receive_data['cmd'] == app_connect_to_ser:
+            message = pickle.loads(conn.recv(self.size_next_mess))
+            if message['cmd'] == app_connect_to_ser:
                 self.conn['sock_app'] = conn
                 self.conn['adr_app'] = adr
-                self.conn['thr_app'] = threading.Thread(target=self.listener, args="app")
-                self.my_queue.append(receive_data)
-            elif receive_data['cmd'] == nb_connect_to_ser:
+                threading.Thread(target=self.listener, args="app").start()
+                self.my_queue.append(message)
+                threading.Thread(target=self.queue_worker()).start()
+            elif message['cmd'] == nb_connect_to_ser:
                 self.conn['sock_nb'] = conn
                 self.conn['adr_nb'] = adr
-                self.conn['thr_nb'] = threading.Thread(target=self.listener)
+                threading.Thread(target=self.listener).start()
             else:
                 conn.close()
 
