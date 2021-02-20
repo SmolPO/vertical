b7167cda0ebf17b7c1ce88e8ebe65cc8642e7e22
user <kalent_ivan@mail.ru>
2021-02-20 17:26:26 +0300

    commit

--- my_helper/notebook/sourse/my_tools.py
+++ my_helper/notebook/sourse/my_tools.py
@@ -1,5 +1,8 @@
-import os
-
+import os, sys
+from PyQt5.QtWidgets import QMessageBox, QPushButton, QApplication
+from PyQt5 import QtWidgets
+from PyQt5 import uic
+import config as conf
 
 def calc_tap_segments():
     d = int(input("Введите диаметр голой трубы"))
@@ -33,3 +36,52 @@ def calc_tap_segments():
     os.startfile("tmp.txt", "print")
     os.remove("tmp.txt")
     return
+
+
+class Notepad(QtWidgets):
+    def __init__(self, parent=None):
+        super(Notepad, self).__init__(parent)
+        self.btn.clicked.connect(self.ev_close)
+        self.text = self.get_text()
+        self.file_note = conf.path + "/notepad.txt"
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Notepad')
+        self.btn = QPushButton('Закрыть', self)
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(50, 50)
+        self.text_area = QtWidgets.QPlainTextEdit()
+        self.text_area.resize(200, 100)
+        self.show()
+
+    def get_text(self):
+        file = open(self.file_note, 'r')
+        text = file.read()
+        file.close()
+        return text
+
+    def set_text(self):
+        text_ui = self.text_area.toPlainText()
+        file = open(self.file_note, 'w')
+        file.write(text_ui + '\n')
+        file.close()
+
+    def ev_close(self):
+        answer = QMessageBox.question(self, 'Вертикаль', "Сохранить записи?",
+                                      QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel, QMessageBox.No)
+        if answer == QMessageBox.Yes:
+            self.set_text()
+            self.close()
+        elif answer == QMessageBox.No:
+            self.close()
+        elif answer == QMessageBox.Cancel:
+            return
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = Notepad()
+    wnd.show()
+    sys.exit(app.exec())
